3ec8e7584a56ffd1bae6b88a13c92b6fa1748085
belmajkarim <belmajkarim29@gmail.com>
2021-05-06 18:22:27 +0200

    feat: Register/Login

---
+++ src/app/account/register.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AccountService} from '../services/account.service';
+import {first} from 'rxjs/operators';
+import {AlertService} from '../services/alert.service';
+
+@Component({ templateUrl: 'register.component.html' })
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) { }
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+
+  // tslint:disable-next-line:typedef
+  get f() { return this.form.controls; }
+
+  // tslint:disable-next-line:typedef
+  onSubmit() {
+    this.submitted = true;
+    this.alertService.clear();
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.accountService.register(this.form.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          this.alertService.success('Registration successful', { keepAfterRouteChange: true });
+          this.router.navigate(['../login'], { relativeTo: this.route });
+        },
+        error: error => {
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      });
+  }
+}
