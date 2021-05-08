1e3508dcd0455fedfba9c85252c3f71cd29543d0
belmajkarim <belmajkarim29@gmail.com>
2021-05-08 17:34:02 +0200

    feat: Login front

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AccountService} from '../../../services/account.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: any;
+  emailCtrl: FormControl;
+  passwordCtrl: FormControl;
+  private errorMessage: string;
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authenticationService: any) {this.emailCtrl = formBuilder.control('', Validators.required);
+                                                   this.passwordCtrl = formBuilder.control('', Validators.required);
+                                                   this.loginForm = formBuilder.group({
+      email: this.emailCtrl,
+      password: this.passwordCtrl,
+    }); }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
+  onSubmit() {
+    this.authenticationService.login(this.loginForm.value).subscribe(user => {
+      localStorage.setItem('token', user.token);
+      localStorage.setItem('user_id', user._id);
+      localStorage.setItem('permissions', user.permissionLevel);
+      this.router.navigate(['home']).then();
+    }, (error) => {
+      switch (error.status) {
+        case 401:
+          this.errorMessage = 'Email or password is wrong!';
+          break;
+        case 400:
+          this.errorMessage = 'Fields can\'t be empty!';
+      }
+    });
+  }
 }
