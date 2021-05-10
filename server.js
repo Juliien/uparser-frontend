8039f1101400c93c58acb3207f0320c67b725caf
Juliien <julien1105@outlook.fr>
2021-05-11 01:49:33 +0200

    fix: update server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/uparser-client'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist//index.html'));
+  res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
 });
 
 app.listen(process.env.PORT);
