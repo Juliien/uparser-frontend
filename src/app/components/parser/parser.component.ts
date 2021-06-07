e2ed6703d966b3cc4584327d861ea3ca56074c79
Juliien <julien1105@outlook.fr>
2021-06-07 12:29:15 +0200

    fix: update parser

--- src/app/components/parser/parser.component.ts
+++ src/app/components/parser/parser.component.ts
@@ -1,12 +1,16 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import * as fileSaver from 'file-saver';
+import {CodeEditorService} from '../../services/code-editor.service';
+import {UserService} from '../../services/user.service';
+import {AuthenticationService} from '../../services/authentication.service';
+import {KafkaModel} from '../../models/kafka.model';
 
 @Component({
   selector: 'app-parser',
   templateUrl: './parser.component.html',
   styleUrls: ['./parser.component.css']
 })
-export class ParserComponent implements AfterViewInit {
+export class ParserComponent implements AfterViewInit, OnInit {
   @ViewChild('editor') editor;
   languages = ['typescript', 'python'];
   themes = ['twilight', 'dracula', 'xcode', 'eclipse'];
@@ -14,12 +18,21 @@ export class ParserComponent implements AfterViewInit {
   selectedTheme = 'twilight';
   isExec = false;
   extensionType: string;
+  file: any;
   exampleCode = `
 function testThis() {
   console.log("it's working!")
 }`;
 
-  constructor() {
+  constructor(private codeEditorService: CodeEditorService,
+              private userService: UserService,
+              private authService: AuthenticationService) {
+  }
+
+  ngOnInit(): void {
+    if (!this.userService.currentUser) {
+      this.userService.getUserByEmail({email: this.authService.decodedToken.sub}).subscribe(user => this.userService.currentUser = user);
+    }
   }
 
   ngAfterViewInit(): void {
@@ -33,10 +46,26 @@ function testThis() {
     this.editor.value = this.exampleCode;
   }
 
-  getUserParserCode(): void {
+  convertFile(): void {
+    // const formData = new FormData();
+    // formData.append('file', this.file.data);
+    // console.log(formData);
     if (this.extensionType) {
+      const data: KafkaModel = {
+        runId: '111',
+        userId: this.userService.currentUser.id,
+        fileName: 'a file',
+        fileContent: 'the file to parse',
+        code: this.editor.value,
+        extensionEnd: this.extensionType,
+        language: this.selectedLang
+      };
+
       this.isExec = true;
-      this.downloadFile();
+      this.codeEditorService.postIntoKafkaTopic(data).subscribe(jsonData => {
+        console.log(jsonData);
+        this.downloadFile();
+      });
       this.isExec = false;
     }
     else {
--- src/app/services/code-editor.service.ts
+++ src/app/services/code-editor.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../models/user.model';
+import {environment} from '../../environments/environment';
+import {KafkaModel} from '../models/kafka.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodeEditorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  postIntoKafkaTopic(formData: KafkaModel): Observable<KafkaModel> {
+    const option = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      })
+    };
+    return this.http.post<KafkaModel>(environment.apiUrl + 'kafka/produce',  formData, option);
+  }
 }
