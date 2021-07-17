7b455e2d67fbe03a17b097bd3294e953e055896a
Juliien <julien1105@outlook.fr>
2021-07-17 20:13:24 +0200

    fix: token expiration

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -16,10 +16,15 @@ export class AuthenticationService {
 
   constructor(private router: Router,
               private http: HttpClient) {
-    if (this.isLogged() && !this.token) {
-      this.token = localStorage.getItem('token');
+    if (this.isLogged()) {
+      if (!this.token) {
+        this.token = localStorage.getItem('token');
+      }
       if (!this.decodedToken) {
         this.decodedToken = this.decodeToken(this.token);
+      }
+      if (!this.isTokenValid()) {
+        this.logout();
       }
     }
   }
@@ -46,6 +51,11 @@ export class AuthenticationService {
   isLogged(): boolean {
     const token = localStorage.getItem('token');
     return token && token.length > 1;
+  }
+
+  isTokenValid(): boolean {
+    const currentDate = Math.round(new Date().getTime() / 1000);
+    return Number(this.decodedToken.exp) > currentDate;
   }
 
   decodeToken(token: string): any {
