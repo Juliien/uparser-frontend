75ba45c1afb55fefc47e2a366fd9bec3eb5d4f80
Juliien <julien1105@outlook.fr>
2021-07-28 03:17:17 +0200

    fix: renames file

--- cypress/integration/register-test.spec.js
+++ cypress/integration/register-test.spec.js
@@ -3,13 +3,13 @@ describe("User", () => {
     cy.visit("/register");
     cy.get(':nth-child(1) > .form-control').type('karim');
     cy.get(':nth-child(2) > .form-control').type('BELMAJDOUB');
-    cy.get(':nth-child(3) > .form-control').type('belmajkarim9@gmail.com');
+    cy.get(':nth-child(3) > .form-control').type('test@gmail.com');
     cy.get(':nth-child(4) > .form-control').type('123456789{enter}');
 
     cy.get('.btn').click();
 
-    //cy.url().should('include', 'localhost:4200/login');
-    //cy.get('.alert').should('contain', 'successful');
+    cy.url().should('include', 'localhost:4200/login');
+    cy.get('.alert').should('contain', 'successful');
 
   });
 });
